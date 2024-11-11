94910212424d19ca487be9ade2c7f6ea7071b5ac
Hero1528 <133805145+Hero1528@users.noreply.github.com>
2024-11-12 00:09:59 +0530

    first commit

---
+++ src/data/user.js
@@ -0,0 +1,66 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by truethari",
+		name: "Tharindu N.",
+		email: "mail@tharindu.dev",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Full-stack web and mobile app developer, and amateur astronaut.",
+		description:
+			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: " PetPal",
+			description:
+				"Developed an Android app offering expert pet care and nutrition advice with static and dynamic content, using Java, XML, and Firebase for user data retrieval and display.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			//linkText: "View Project",
+			//link: "https://github.com",
+		},
+
+		{
+			title: "Scan and Order",
+			description:
+				"Developed a 2022-2023 hotel menu scanner app allowing users to scan menus, place orders, and view bills for a seamless dining experience.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			//linkText: "View Project",
+			//link: "https://github.com",
+		},
+
+		{
+			title: "Face Part Dection",
+			description:
+				"It is a python-based project which can scan and detect the face parts of humans. it can detect various face parts like eyes, ears, nose, etc.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			//linkText: "View Project",
+			//link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
